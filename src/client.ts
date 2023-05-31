a516e31d5675121450c3521fae9a16b12d624b13
samlior <samlior@foxmail.com>
2023-05-31 11:25:43 +0800

    chore: improve client

--- src/client.ts
+++ src/client.ts
@@ -5,7 +5,6 @@ import process from "process";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import Socks5ClientSocket from "socks5-client/lib/Socket";
-import * as messages from "./messages";
 
 (async function () {
   // parse args
@@ -71,117 +70,24 @@ import * as messages from "./messages";
     number,
     {
       socket: net.Socket;
-      write: Buffer[];
-      read: (Buffer | "close")[];
+      queue: (Buffer | "close")[];
       connected: boolean;
     }
   >();
 
-  // upstream socket instance
-  let up: net.Socket | undefined;
-  let connecting = false;
-  setInterval(() => {
-    if (up === undefined && !connecting) {
-      // change connecting flag
-      connecting = true;
+  // upstream sockets
+  const ups = new Map<number, { socket: net.Socket }>();
 
-      // create upstream
-      const _up = createSocket()
-        .connect(args.upstreamPort, args.upstreamIp)
-        .setKeepAlive(true);
+  // pending upstream socket
+  let pending: net.Socket | undefined = undefined;
 
-      const handleConnect = () => {
-        // remove listeners
-        _up.off("connect", handleConnect);
-        _up.off("error", handleConnectError);
+  // auto increment id
+  let autoId = 0;
+  const getId = () => {
+    return ++autoId;
+  };
 
-        // pending data
-        let length = 0;
-        let id = 0;
-        let pending = Buffer.alloc(0);
-
-        _up.on("data", (data) => {
-          if (data.length > 1 && data[0] === 5) {
-            console.log("ignore socks5 data...");
-            return;
-          }
-
-          while (data.length > 0) {
-            if (length === 0) {
-              if (data.length < 8) {
-                console.log("upstream data is less than 8 bytes, ignore");
-                return;
-              }
-              // init pending data
-              id = data.readInt32BE();
-              length = data.readInt32BE(4);
-              pending = Buffer.alloc(0);
-
-              // slice data, ignore header
-              data = data.subarray(8);
-            }
-
-            if (length > 0) {
-              // append pending data
-              const _length = length > data.length ? data.length : length;
-              const _data = data.subarray(0, _length);
-              length -= _length;
-              pending = Buffer.concat([pending, _data]);
-              data = data.subarray(_length);
-            }
-
-            if (length === 0) {
-              if (downs.has(id)) {
-                writeToDownstream(id, data);
-              } else {
-                console.log("incoming upstream connection", id);
-                createDown(id, data);
-              }
-
-              // clear pending data
-              id = 0;
-              pending = Buffer.alloc(0);
-            }
-          }
-        });
-
-        _up.on("close", () => {
-          console.log("lose upstream connection");
-          up = undefined;
-        });
-        _up.on("error", (err) => {
-          console.log("upstream connection error:", err);
-          _up.destroy(); // safety off
-          up = undefined;
-        });
-        console.log("successfully connect with upstream server");
-        // save upstream instance
-        up = _up;
-
-        // clear queue
-        for (const [id, { read }] of downs) {
-          if (read.length > 0) {
-            writeToUpstream(id);
-          }
-        }
-
-        // change connecting flag
-        connecting = false;
-      };
-      const handleConnectError = (err: Error) => {
-        console.log("(connecting)upstream connection error:", err);
-        _up.destroy(); // safety off
-
-        // change connecting flag
-        connecting = false;
-      };
-
-      _up.once("connect", handleConnect);
-      _up.once("error", handleConnectError);
-    }
-  }, 1000);
-
-  const writeToDownstream = (id: number, data?: Buffer) => {
+  const writeToDownstream = (id: number, data?: Buffer | "close") => {
     const down = downs.get(id);
     if (!down) {
       // ignore
@@ -189,64 +95,107 @@ import * as messages from "./messages";
     }
 
     if (data) {
-      down.write.push(data);
+      down.queue.push(data);
     }
 
-    if (down.connected) {
-      while (down.write.length > 0) {
-        const data = down.write.shift()!;
-        if (data.equals(messages.close)) {
-          console.log("close from upstream", id);
-          downs.delete(id);
-          down.socket.destroy();
-          break;
-        } else {
-          console.log("write", data.length, "bytes for", id);
-          down.socket.write(data);
-        }
+    while (down.connected && down.queue.length > 0) {
+      const data = down.queue.shift()!;
+      if (data === "close") {
+        console.log("close from upstream", id);
+        downs.delete(id);
+        down.socket.destroy();
+        break;
+      } else {
+        console.log("write", data.length, "bytes for", id);
+        down.socket.write(data);
       }
     }
   };
 
-  const writeToUpstream = (id: number, data?: Buffer | "close") => {
-    const down = downs.get(id);
-    if (!down) {
-      // ignore
-      return;
-    }
-
-    // push to queue
-    if (data) {
-      down.read.push(data);
-    }
-
+  const writeToUpstream = (id: number, data: Buffer | "close") => {
+    const up = ups.get(id);
     if (!up) {
       // ignore
       return;
     }
 
-    while (down.read.length > 0) {
-      const data = down.read.shift()!;
-      const content = data === "close" ? messages.close : data;
-      const upsteamData = Buffer.concat([Buffer.alloc(8), content]);
-      upsteamData.writeInt32BE(id);
-      upsteamData.writeInt32BE(content.length, 4);
-      up.write(upsteamData);
-      if (data === "close") {
-        console.log("close from downstream", id);
-      } else {
-        console.log("reply", data.length, "bytes for", id);
-      }
+    if (data === "close") {
+      console.log("close from downstream", id);
+      ups.delete(id);
+      up.socket.destroy();
+    } else {
+      console.log("reply", data.length, "bytes for", id);
+      up.socket.write(data);
     }
+  };
+
+  const createPending = () => {
+    if (pending !== undefined) {
+      // safety check
+      return;
+    }
+
+    let id: number | undefined = undefined;
+    const _pending = createSocket()
+      .connect(args.upstreamPort, args.upstreamIp)
+      .setKeepAlive(true);
+    _pending.on("connect", () => {
+      _pending.on("data", (data) => {
+        if (id === undefined) {
+          id = getId();
+          ups.set(id, { socket: _pending });
+
+          // create a new pending socket now
+          pending = undefined;
+          createPending();
+        }
+
+        if (ups.has(id)) {
+          if (downs.has(id)) {
+            writeToDownstream(id, data);
+          } else {
+            createDown(id, data);
+          }
+        }
+      });
+      _pending.on("close", () => {
+        if (id !== undefined) {
+          console.log("upstream closed", id);
+          writeToDownstream(id, "close");
+        } else {
+          // maybe something is wrong,
+          // sleep a while and reconnect
+          pending = undefined;
+          if (!closing) {
+            setTimeout(() => createPending(), 1000);
+          }
+        }
+      });
+    });
+    _pending.on("error", (err) => {
+      if (id !== undefined) {
+        console.log("upstream id:", id, "error:", err);
+        writeToDownstream(id, "close");
+      } else {
+        console.log("pending upstream error:", err);
+        // maybe something is wrong,
+        // sleep a while and reconnect
+        pending = undefined;
+        if (!closing) {
+          setTimeout(() => createPending(), 1000);
+        }
+      }
+    });
+    // save pending socket object
+    pending = _pending;
   };
 
   const createDown = (id: number, data: Buffer) => {
     const _down = new net.Socket()
       .connect(args.downstreamPort, args.downstreamIp)
       .setKeepAlive(true);
-    const write: Buffer[] = [data];
-    const read: (Buffer | "close")[] = [];
-    const ctx = { socket: _down, write, read, connected: false };
+    const queue: (Buffer | "close")[] = [data];
+    const ctx = { socket: _down, queue, connected: false };
     downs.set(id, ctx);
     _down.on("connect", () => {
       if (!downs.has(id)) {
@@ -264,15 +213,14 @@ import * as messages from "./messages";
         if (downs.has(id)) {
           console.log("downstream closed", id);
           writeToUpstream(id, "close");
-          downs.delete(id);
         }
       });
-      _down.on("error", () => {
-        if (downs.has(id)) {
-          writeToUpstream(id, "close");
-          downs.delete(id);
-        }
-      });
+    });
+    _down.on("error", (err) => {
+      if (downs.has(id)) {
+        console.log("downstream id:", id, "error:", err);
+        writeToUpstream(id, "close");
+      }
     });
   };
 
@@ -284,10 +232,13 @@ import * as messages from "./messages";
       console.log("client is closing...");
 
       // close all sockets
-      if (up) {
-        up.destroy();
+      if (pending) {
+        pending.destroy();
       }
       for (const [, { socket }] of downs) {
+        socket.destroy();
+      }
+      for (const [, { socket }] of ups) {
         socket.destroy();
       }
 
